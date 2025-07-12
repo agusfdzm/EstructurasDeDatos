ca734f16331f4a2d1348e2dc00bd754d73eb5f59
agusfdzm <129664400+agusfdzm@users.noreply.github.com>
2025-07-12 18:58:35 +0200

    Creación de la LinkedList

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -1,28 +1,104 @@
-class Nodo:
-    def __init__(self, dato):
-        self.dato = dato
-        self.siguiente = None
+from dataclasses import dataclass
 
-# nodo1 = Nodo(1)
-# nodo2 = Nodo(2)
-# nodo3 = Nodo(3)
-
-# nodo1.siguiente = nodo2
-# nodo2.siguiente = nodo3
-# nodo3.siguiente = None
+@dataclass
+class Node:
+    value: int
+    nextNode: "Node"
 
 class LinkedList:
-    def __init__(self):
-        self.cabeza = None
+    _size: int
+    _firstNode: Node
 
-    def agregar_al_inicio(self, dato):
-        nuevo = Nodo(dato)
-        nuevo.siguiente = self.cabeza
-        self.cabeza = nuevo
+    def __init__(self) -> None:
+        self._size = 0
+        self._firstNode = None
 
-    def mostrar(self):
-        actual = self.cabeza
-        while actual:
-            print(actual.dato, end=" -> ")
-            actual = actual.siguiente
-        print("None")
+    def add(self, position: int, element: int):
+        if position == 0:
+            self._firstNode = Node(value=element, nextNode=self._firstNode)
+        else:
+            currentNode = self._firstNode
+            for _ in range(0, position - 1):
+                currentNode = currentNode.nextNode
+            currentNode.nextNode = Node(
+                value=element, nextNode=currentNode.nextNode
+            )
+
+        self._size += 1
+
+    def delete(self, position: int):
+        if position == 0:
+            self._firstNode = self._firstNode.nextNode
+        else:
+            currentNode = self._firstNode
+            for _ in range(0, position - 1):
+                currentNode = currentNode.nextNode
+            currentNode.nextNode = currentNode.nextNode.nextNode
+
+    def get(self, position: int) -> int:
+        currentNode = self._firstNode
+        for _ in range(0, position):
+            currentNode = currentNode.nextNode
+        return currentNode.value
+    
+    def recibirTamaño(self) -> int:
+        return self._size
+    
+ejemplo = LinkedList()
+ejemplo.add(0, 1)
+print(ejemplo.recibirTamaño())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Nodo:
+#     def __init__(self, dato):
+#         self.dato = dato
+#         self.siguiente = None
+
+# # nodo1 = Nodo(1)
+# # nodo2 = Nodo(2)
+# # nodo3 = Nodo(3)
+
+# # nodo1.siguiente = nodo2
+# # nodo2.siguiente = nodo3
+# # nodo3.siguiente = None
+
+# class LinkedList:
+#     def __init__(self):
+#         self.cabeza = None
+
+#     def agregar_al_inicio(self, dato):
+#         nuevo = Nodo(dato)
+#         nuevo.siguiente = self.cabeza
+#         self.cabeza = nuevo
+
+#     def mostrar(self):
+#         actual = self.cabeza
+#         while actual:
+#             print(actual.dato, end=" -> ")
+#             actual = actual.siguiente
+#         print("None")
